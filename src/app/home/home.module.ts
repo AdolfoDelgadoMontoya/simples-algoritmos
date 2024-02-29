9078d3255d2d06eb5c46080e28a34e84de7b8a60
AdolfoDelgadoMontoya <polybius1@outlook.es>
2024-02-29 13:45:52 -0600

    correccion posicion elementos correctamente

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -16,4 +16,35 @@ import { HomePageRoutingModule } from './home-routing.module';
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+export class HomePageModule {
+  public grafica = {
+    data:[{
+      x:[1,2,3], y:[5,6,7],type:'bar'
+    }],
+  }
+  ngOnInit(){
+    this.poblar();
+  }
+  /*
+  si ngOnInit aparece en amarillo, debemos de agregarle:void
+  si no se tiene la funcion  se agrega:
+  
+  */
+  poblar(){
+    this.poblarX(100);
+    this.poblarY(100);
+  }
+
+  poblarX(num:number){
+    for(let i=0;i<num;i++){
+      this.grafica.data[0].x[i]=i;
+    }
+  }
+
+  poblarY(num:number){
+    for(let i=0;i<num;i++){
+      this.grafica.data[0].x[i]=
+      Math.floor(Math.random()*num)+1;
+    }
+  } 
+}
--- src/app/services/algoritmos-simples.service.ts
+++ src/app/services/algoritmos-simples.service.ts
@@ -12,40 +12,7 @@ PlotlyModule.plotlyjs = PlotlyJS;
 })
 export class AlgoritmosSimplesService {
 
-  public grafica = {
-    data:[{
-      x:[1,2,3], y:[5,6,7],type:'bar'
-    }],
-  }
-
   constructor() { }
-
-  ngOnInit(){
-    this.poblar();
-  }
-  /*
-  si ngOnInit aparece en amarillo, debemos de agregarle:void
-  si no se tiene la funcion  se agrega:
-  
-  */
-
-  poblar(){
-    this.poblarX(100);
-    this.poblarY(100);
-  }
-
-  poblarX(num:number){
-    for(let i=0;i<num;i++){
-      this.grafica.data[0].x[i]=i;
-    }
-  }
-
-  poblarY(num:number){
-    for(let i=0;i<num;i++){
-      this.grafica.data[0].x[i]=
-      Math.floor(Math.random()*num)+1;
-    }
-  }
 
   esprimo(n:number):boolean{
     if(n<=1){
