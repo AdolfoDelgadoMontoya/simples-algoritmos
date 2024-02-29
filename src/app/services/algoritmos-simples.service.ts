84b33afcb7690c87b608c0e9f01a6a9e562f7228
AdolfoDelgadoMontoya <polybius1@outlook.es>
2024-02-29 13:25:43 -0600

    agregar arreglo grafica

--- src/app/services/algoritmos-simples.service.ts
+++ src/app/services/algoritmos-simples.service.ts
@@ -12,7 +12,33 @@ PlotlyModule.plotlyjs = PlotlyJS;
 })
 export class AlgoritmosSimplesService {
 
+  public grafica = {
+    data:[{
+      x:[1,2,3], y:[5,6,7],type:'bar'
+    }],
+  }
+
   constructor() { }
+
+  ngOnInit(){
+  }
+
+  poblar(){
+    this.poblarX(100);
+    this.poblarY(100);
+  }
+
+  poblarX(num:number){
+    for(let i=0;i<num;i++){
+      this.grafica.data[0].x[i]=i;
+    }
+  }
+
+  poblarY(num:number){
+    for(let i=0;i<num;i++){
+      this.grafica.data[0].x[i]=i;
+    }
+  }
 
   esPrimo(n:number):boolean{
     if(n<=1){
