fdca9d349fc80077e2f26ee8da52ac36685dc10b
AdolfoDelgadoMontoya <polybius1@outlook.es>
2024-03-05 13:25:48 -0600

    5-3-2024

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,8 @@ import * as PlotlyJS from 'plotly.js-dist-min';
 import { PlotlyModule } from 'angular-plotly.js';
 PlotlyModule.plotlyjs = PlotlyJS;
 
+import { AlgoritmosSimplesService } from '../services/algoritmos-simples.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -11,16 +13,14 @@ PlotlyModule.plotlyjs = PlotlyJS;
 })
 export class HomePage {
   public grafica = {data:[{x:[1,2,3],y:[5,6,7],type:'bar'}],layout:{title:'',width:500,height:500}};
-  constructor() {}
+  constructor(private miObjeto:AlgoritmosSimplesService) {}
   
   ngOnInit(){
     this.poblar();
+    this.bubbleSort();
+    this.generar();
   }
-  /*
-  si ngOnInit aparece en amarillo, debemos de agregarle:void
-  si no se tiene la funcion  se agrega:
   
-  */
   poblar(){
     this.poblarX(100);
     this.poblarY(100);
@@ -28,8 +28,27 @@ export class HomePage {
 
   poblarX(num:number){
     for(let i=0;i<num;i++){
+    
       this.grafica.data[0].x[i]=i;
     }
+  }
+
+  bubbleSort(){
+    let checar;
+    do{
+      checar=false;
+      for(let i=0;i < this.grafica.data[0].y.length;i++){
+        if(this.grafica.data[0].y[i]>this.grafica.data[0].y[i+1]){
+          let tmp = this.grafica.data[0].y[i];
+          this.grafica.data[0].y[i]=this.grafica.data[0].y[i+1]
+          this.grafica.data[0].y[i+1]=tmp;
+          checar=true;
+        }
+      }
+    }while(checar);
+  }
+
+  generar(){
   }
 
   poblarY(num:number){
